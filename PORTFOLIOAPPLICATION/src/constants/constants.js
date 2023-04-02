2a91d097dd85754c4449eac61229971f810365de
coderrohitmohan <100717589+coderrohitmohan@users.noreply.github.com>
2023-04-02 16:12:35 +0530

    PORTFOLIOAPPLICATION

---
+++ PORTFOLIOAPPLICATION/src/constants/constants.js
@@ -0,0 +1,45 @@
+export const projects = [
+  {
+    title: 'MERN Memories',
+    description: "write any thing",
+      image: '/images/1.png',
+      tags: ['Mongo', 'Express', 'React', 'Node'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 0,
+  },
+  {
+    title: 'E-Commerce',
+    description:"write any thing",
+    image: '/images/2.png',
+    tags: ['React', 'JavaScript'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 1,
+  },
+  {
+    title: 'WebRTC App',
+    description: "write any thing",
+      image: '/images/3.jpg',
+      tags: ['React', 'WebRTC'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 2,
+  },
+  {
+    title: 'Unichat',
+    description: "write any thing",
+    tags: ['React', 'ChatEngine', 'Firebase'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2020, text: 'Started my journey', },
+  { year: 2021, text: 'Had interest in UI/UX Designing', },
+  { year: 2022, text: 'Founded JavaScript and React to be so great and easy to write', },
+  { year: 2023, text: 'Strarted Exploring and sharing Projects in College & Esteemed companies ', },
+  { year: 2024, text: 'Hoping for a better opportunity', },
+];
